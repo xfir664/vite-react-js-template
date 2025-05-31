3a83081cce50dd17efe76824e216f429080ea160
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2025-05-31 15:01:39 +0300

    start

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/deploy-test/", //change repo name
   plugins: [react()],
   resolve: {
     alias: {
